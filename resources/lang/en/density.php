65e183befa9221ccd13b3fcb9be9c1b2f5b89efa
Ahmed Dabak <ahmed-dabak@hotmail.com>
2021-08-25 17:38:45 +0200

    add density

---
+++ resources/lang/en/density.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'kg/m^3' => 'Kilogram/Cubic meter',
+    't/m^3' => 'Ton/Cubic meter',
+    'lb/ft^3' => 'Pound/Cubic foot',
+];
---
+++ src/Units/Density.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LasePeCo\UnitConverter\Units;
+
+class Density
+{
+    public const KilogramPerCubicMeter = 'kg/m^3';
+    public const TonPerCubicMeter = 't/m^3';
+    public const PoundPerCubicFoot = 'lb/ft^3';
+}
