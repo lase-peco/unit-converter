17a34d93e06005466c665eb6e0c5f34cee2f79aa
abdulsalamemesh <abdulsalam.emesh.work@gmail.com>
2022-02-16 16:43:08 +0100

    add VolumetricFlowRate Unit class

---
+++ src/Units/VolumetricFlowRate.php
@@ -0,0 +1,10 @@
+<?php
+namespace LasePeCo\UnitConverter\Units;
+
+class VolumetricFlowRate
+{
+    public const CubicDecimeterPerHour = 'dm^3/h';
+    public const CubicMeterPerHour = 'm^3/h';
+    public const CubicFootPerHour = 'ft^3/h';
+    public const CubicYardPerHour = 'yd^3/h';
+}
