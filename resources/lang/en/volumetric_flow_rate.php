648470b1361878a7ca883e515f2e4a6f7d7b82a9
abdulsalamemesh <abdulsalam.emesh.work@gmail.com>
2022-02-17 09:35:33 +0100

    add translation for volumetric flow rate

---
+++ resources/lang/en/volumetric_flow_rate.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dm^3/h' => 'Cubic decimeter per hour',
+    'm^3/h' => 'Cubic meter per hour',
+    'ft^3/h' => 'Cubic foot per hour',
+    'yd^3/h' => 'Cubic yard per hour',
+];
